aef77bf74d8baec167b20e62e2d2d9976ccc7976
Nathan Jackson <nathan@forallsecure.com>
2022-05-09 13:16:19 -0400

    add solution for cereal exercise

---
+++ cereal/solution.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <cstdint>
+
+#include <cereal/archives/json.hpp>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    std::string s(data, data + size);
+
+    std::stringstream ss;
+
+    std::string s2;
+
+    // Note: we have to wrap writing to the output archive because the data
+    // isn't flushed to stringstream until it goes out of scope.
+    {
+        cereal::JSONOutputArchive oa(ss);
+        oa(s);
+    }
+
+    cereal::JSONInputArchive ia(ss);
+    ia(s2);
+
+    // assert(s == s2);
+
+    return 0;
+}
+
